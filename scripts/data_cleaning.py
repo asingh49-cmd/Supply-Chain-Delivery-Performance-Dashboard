68f6834d5e966694e7c468e70591d3f25867adc3
Aditya Singh <adityapsingh1815@gmail.com>
2025-12-30 20:55:41 -0600

    Fixed data cleaning errors and translated columns in spanish

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from easynmt import EasyNMT
 #Load dataset
 data = pd.read_csv('data/raw/DataCoSupplyChainDataset.csv', encoding='latin1',low_memory=False)
 
@@ -25,10 +25,31 @@ date_columns = ['order_date_dateorders','shipping_date_dateorders']
 for col in date_columns:
     data[col] = pd.to_datetime(data[col],errors='raise')
 
-#Drop product description and order zipcode column due to high missing values
-columns_to_drop = ['product_description','order_zipcode']
+#Translate order_country and order_state columns to English
+translator = EasyNMT('m2m_100_418M')
+
+def translate_es_to_en(series):
+    s = series.dropna().astype(str).str.strip()
+    unique_vals = s.unique()
+
+    print(f"Translating {len(unique_vals)} unique values")
+
+    translated = translator.translate(
+        list(unique_vals),
+        source_lang="es",
+        target_lang="en",
+        batch_size=32
+    )
+    mapping = dict(zip(unique_vals, translated))
+    return series.astype(str).str.strip().map(mapping)
+
+data['order_country'] = translate_es_to_en(data['order_country'])
+data['order_state'] = translate_es_to_en(data['order_state'])
+
+#Drop product description and order zipcode column due to high missing values and other irrelevant columns
+columns_to_drop = ['product_description','order_zipcode','customer_email','customer_password','product_image']
 data = data.drop(columns=columns_to_drop)
-print("Dropped 'product_description' and 'order_zipcode' columns due to high missing values")
+print("Dropped 'product_description' and 'order_zipcode' columns due to high missing values and email, password and product image for irrelevance.")
 # Fill missing values for numerical columns with median
 numerical_cols = data.select_dtypes(include=[np.number]).columns
 data[numerical_cols] = data[numerical_cols].fillna(data[numerical_cols].median)
@@ -55,4 +76,4 @@ print("Feature engineering completed")
 #Save cleaned data
 print("Saving cleaned data...")
 data.to_csv('data/processed/cleaned_dataco_supply_chain_dataset.csv',index=False)
-print(f"Cleaned data saved with shape: {len(data)} rows and {len(data.columns)} columns") #180519 rows and 59 columns
+print(f"Cleaned data saved with shape: {len(data)} rows and {len(data.columns)} columns") #180519 rows and 56 columns
